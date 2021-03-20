960c09a0ba49699b50179828e57e4e72b1a469af
sansy98 <60301258+sansy98@users.noreply.github.com>
2021-03-20 01:02:52 +0100

    Fully functional map editor implemented

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from pygame.locals import *
 import sys
-
+from scripts.map_editor import toggle_map_editor
 
 def load_map(path):
     f = open('maps/' + path + '.txt', 'r')
@@ -12,6 +12,14 @@ def load_map(path):
     for line in data:
         game_map.append(list(line))
     return game_map
+
+def save_map(path, game_map):
+    f = open('maps/' + path + '.txt', 'w')
+    for line in game_map:
+        f.write('\n')
+        for num in line:
+            f.write(num)
+    f.close()
 
 def collision_test(rect, tiles):
     hit_list = []
@@ -42,7 +50,6 @@ def move(rect, movement, tiles):
         elif movement[1] < 0:
             rect.top = tile.bottom
             collision_states['top'] = True
-    
     return rect, collision_states
 
 
@@ -78,6 +85,12 @@ if __name__ == "__main__":
 
     precise_scroll = [player_rect.x, player_rect.y]
     
+    EDITOR_active = False
+    EDITOR_current_tile = 0
+    EDITOR_isMouseClicked = False
+    EDITOR_moving_up = False
+    EDITOR_moving_down = False
+    TILE_TYPES = 2
 
     #Debug vars, ignore these
     #debug_var = 0
@@ -102,6 +115,20 @@ if __name__ == "__main__":
                     moving_left = True
                 if event.key == K_SPACE and onair_timer == 0:    
                     jumping = True
+                ##Editor inputs
+                if event.key == K_m and not EDITOR_active:
+                    EDITOR_active = True
+                elif event.key == K_m and EDITOR_active:
+                    EDITOR_active = False
+                    save_map("0_1", game_map)                   #Save map
+                    precise_scroll[0] = 0                       #Reset camera
+                    precise_scroll[1] = 0
+                    player_rect.x = 3*TILE_SIZE                 #Reset player position
+                    player_rect.y = 2*TILE_SIZE
+                if event.key == K_w and EDITOR_active:
+                    EDITOR_moving_up = True
+                if event.key == K_s and EDITOR_active:
+                    EDITOR_moving_down = True
 
             if event.type == KEYUP:
                 if event.key == K_d:
@@ -110,42 +137,78 @@ if __name__ == "__main__":
                     moving_left = False
                 if event.key == K_SPACE and jumping:
                     jumping = False
+                ##Editor inputs
+                if event.key == K_w:
+                    EDITOR_moving_up = False
+                if event.key == K_s:
+                    EDITOR_moving_down = False
+
+            if event.type == MOUSEBUTTONDOWN:
+                ##Editor inputs
+                if event.button == 1 and EDITOR_active:
+                    EDITOR_isMouseClicked = True
+                if event.button == 3 and EDITOR_active:
+                    if EDITOR_current_tile == TILE_TYPES:
+                        EDITOR_current_tile = 0
+                    else:
+                        EDITOR_current_tile += 1
 
         #CAMERA------------------------------------------#
-        precise_scroll[0] += (player_rect.x-precise_scroll[0]-(454/2 + 4))/20
-        precise_scroll[1] += (player_rect.y-precise_scroll[1]-(LOCAL_RESOLUTION[1]/2 + 4))/20
+        if not EDITOR_active:
+            precise_scroll[0] += (player_rect.x-precise_scroll[0]-(454/2 + 4))/20
+            precise_scroll[1] += (player_rect.y-precise_scroll[1]-(LOCAL_RESOLUTION[1]/2 + 4))/20
+        else:
+            precise_scroll[0] += player_rect.x-precise_scroll[0]-(454/2 + 4)
+            precise_scroll[1] += player_rect.y-precise_scroll[1]-(LOCAL_RESOLUTION[1]/2 + 4)
         scroll = precise_scroll.copy()
         scroll[0] = int(scroll[0])
         scroll[1] = int(scroll[1])
 
         #DRAWING-----------------------------------------#
+        mapped_tiles = []
         tangible_tiles = []
         y=0
         for layer in game_map:
             x=0
+            mapped_tiles.append([])
             for tile in layer:
+                mapped_tiles[y].append(Rect(x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1], TILE_SIZE, TILE_SIZE))
                 if tile == '1': 
                     SCREEN.blit(dirt_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
                 elif tile == '2': 
                     try:
-                        if game_map[y][x+1] == '0' or x == len(layer)-1:
+                        if game_map[y][x+1] == '0':
                             SCREEN.blit(grassCOR_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
                     except IndexError:
-                        SCREEN.blit(grassCOR_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                        if x == len(layer)-1 and game_map[y][x-1] == '0':
+                            SCREEN.blit(pg.transform.flip(grassCOR_img, True, False), (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                        elif x == len(layer)-1:
+                            SCREEN.blit(grassCOR_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
                     try:
                         if game_map[y][x-1] == '0':
                             SCREEN.blit(pg.transform.flip(grassCOR_img, True, False), (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
                     except IndexError:
-                        SCREEN.blit(pg.transfrom.flip(grassCOR_img, True, False), (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
-                    if (game_map[y][x-1] == '0' and game_map[y][x+1] == '0') or (game_map[y][x-1] != '0' and game_map[y][x+1] != '0'):
-                        SCREEN.blit(grass_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
-                if tile != '0':
+                        if x == 0 and game_map[y][x+1] == '0':
+                            SCREEN.blit(grassCOR_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                        elif x == 0:
+                            SCREEN.blit(pg.transform.flip(grassCOR_img, True, False), (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                    try:
+                        if (game_map[y][x-1] == '0' and game_map[y][x+1] == '0') or (game_map[y][x-1] != '0' and game_map[y][x+1] != '0'):
+                            SCREEN.blit(grass_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                        if x == 0 and game_map[y][x+1] == '0':
+                            SCREEN.blit(grassCOR_img, (x*TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                    except IndexError:
+                        pass
+                if tile != '0' and not EDITOR_active:
                     tangible_tiles.append(Rect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
                 x+=1
             y+=1
 
         #Movement & Collision--------------------------#
         player_movement = [0, 0]
+        if not EDITOR_active:
+            player_last_place = player_movement
+
         if moving_right:
             player_movement[0] += 2
             last_orientation = True
@@ -153,41 +216,64 @@ if __name__ == "__main__":
             player_movement[0] -= 2
             last_orientation = False
 
-        player_movement[1] += player_ymomentum
-        if jumping:
-            if onair_timer == 0:
-                player_ymomentum = -2
-            player_ymomentum += -0.2
-        else: player_ymomentum += 0.2
-        if onair_timer > 8:
-            jumping = False
-        if player_ymomentum > 3:
-            player_ymomentum = 3
-        
-        player_rect, collisions = move(player_rect, player_movement, tangible_tiles)
-
-        if collisions['bottom'] and onair_timer > 0:
-            onair_timer = 0
-            jumping = False
-            player_ymomentum = 1
-        elif not collisions['bottom']:
-            onair_timer += 1
-
-        if collisions['top']:    
-            player_ymomentum = 1
-        
-        if moving_right:
-            SCREEN.blit(player_img, (player_rect.x-scroll[0], player_rect.y-scroll[1]))
-        elif moving_left:
-            SCREEN.blit(pg.transform.flip(player_img, True, False), (player_rect.x-scroll[0], player_rect.y-scroll[1]))
+        if not EDITOR_active:
+            player_movement[1] += player_ymomentum
+            if jumping:
+                if onair_timer == 0:
+                    player_ymomentum = -2
+                player_ymomentum += -0.2
+            else: player_ymomentum += 0.2
+            if onair_timer > 8:
+                jumping = False
+            if player_ymomentum > 3:
+                player_ymomentum = 3
         else:
-            if last_orientation:
+            try:
+                if EDITOR_moving_up:
+                    player_movement[1] -= 2
+                if EDITOR_moving_down:
+                    player_movement[1] += 2
+            except NameError:
+                print("EDITOR_moving_* not yet defined, skipping...")
+
+        player_rect, collisions = move(player_rect, player_movement, tangible_tiles)
+        
+        if not EDITOR_active:
+            if collisions['bottom'] and onair_timer > 0:
+                onair_timer = 0
+                jumping = False
+                player_ymomentum = 1
+            elif not collisions['bottom']:
+                onair_timer += 1
+
+            if collisions['top']:    
+                player_ymomentum = 1
+            
+        ##Draw player or editor surface
+        if not EDITOR_active:
+            if moving_right:
                 SCREEN.blit(player_img, (player_rect.x-scroll[0], player_rect.y-scroll[1]))
-            else:
+            elif moving_left:
                 SCREEN.blit(pg.transform.flip(player_img, True, False), (player_rect.x-scroll[0], player_rect.y-scroll[1]))
+            else:
+                if last_orientation:
+                    SCREEN.blit(player_img, (player_rect.x-scroll[0], player_rect.y-scroll[1]))
+                else:
+                    SCREEN.blit(pg.transform.flip(player_img, True, False), (player_rect.x-scroll[0], player_rect.y-scroll[1]))
+        if EDITOR_active:
+            ##Map editor related code (Update map info and add screen elements)
+            try:
+                EDITOR_surface, game_map = toggle_map_editor(game_map, EDITOR_isMouseClicked, EDITOR_current_tile, scroll, mapped_tiles)
+            except NameError:
+                EDITOR_surface, game_map = toggle_map_editor(game_map, None, EDITOR_current_tile, scroll, mapped_tiles)
+            EDITOR_isMouseClicked = False
 
         #SCREEN & TIMING UPDATES----------------------------#
-
+        if EDITOR_active:
+            try:
+                SCREEN.blit(EDITOR_surface, (0,0))
+            except NameError:
+                print("EDITOR_surface not yet defined, skipping...")
         pg.transform.scale(SCREEN, RESOLUTION, FSCREEN)
         pg.display.update()
         CLOCK.tick(60)
@@ -197,5 +283,6 @@ if __name__ == "__main__":
 #        if collisions['top']:
 #            pg.image.save(FSCREEN, f"debug/{debug_var}.png")
 #            debug_var += 1
-        
+#        print(EDITOR_current_tile)       
+    
     pg.quit()
---
+++ scripts/map_editor.py
@@ -0,0 +1,48 @@
+import pygame as pg
+from pygame.locals import *
+import os, sys
+from math import floor
+
+
+def toggle_map_editor(game_map, isMouseClicked, current_tile, scroll, mapped_tiles):
+    editor_surface = pg.Surface((455, 270))
+    display_font = pg.font.SysFont('Helvetica', 15)
+    text_surface = display_font.render(f"Current tile: {current_tile}", False, (255, 0, 0))
+    cells = []
+    
+    #Edit tile
+    if isMouseClicked:
+        raw_mouse_pos = pg.mouse.get_pos()
+        mouse_pos = ((raw_mouse_pos[0]*455)/1280, (raw_mouse_pos[1]*270)/800)
+        tile_found = False
+        try:
+            y=0
+            for layer in mapped_tiles:
+                x=0
+                for tile in layer:
+                    if tile.collidepoint(mouse_pos):
+                        tile_found = True
+                        game_map[y][x] = str(current_tile)
+                        break
+                    x+=1
+                if tile_found:
+                    break
+                y+=1
+        except IndexError:
+            print(f"game_map[{y}][{x}] out of range!")
+            IndexErrored = True
+        else:
+            IndexErroed = False
+        if IndexErroed:
+            raise IndexError
+
+    #Draw grid
+    for y in range(0, len(game_map)):
+        pg.draw.rect(editor_surface, (100, 100, 100), pg.Rect(-scroll[0], y*16-scroll[1], len(game_map[0])*16, 1))
+    for x in range(0, len(game_map[0])+1):
+        pg.draw.rect(editor_surface, (100, 100, 100), pg.Rect(x*16-scroll[0], -scroll[1], 1, (len(game_map)*16)-15))
+    
+    #Add selected tile text to returned surface
+    editor_surface.blit(text_surface, (0,0))
+    editor_surface.set_colorkey((0, 0, 0))
+    return editor_surface, game_map
