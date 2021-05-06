017fa642941212b19d8d7ad92314f80394b2900f
sansy98 <60301258+sansy98@users.noreply.github.com>
2021-05-06 11:16:52 +0200

    HP_System

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,9 +8,25 @@ class Entity:
 		self.y = y
 		self.sprite = sprite   #Surface type
 		self.rect = pg.Surface.get_rect(sprite)
+		self.movement = [0, 0]
+		self.direction = 0		#0: left, 1: right
+		self.collisions = {'top': False, 'bottom': False, 'right': False, 'left': False}
+		self.rect.x = self.x
+		self.rect.y = self.y
 
 	def move(self):
-		self.x -= 1
+		if self.direction == 0:
+			self.movement[0] = -1
+		elif self.direction == 1:
+			self.movement[0] = 1
+		if not self.collisions['bottom']:
+			self.movement[1] = 2
+		else:
+			self.movement[1] = 0
 
 	def update(self):
-		pass
+		if self.collisions['left'] and self.direction == 0:
+			self.direction = 1
+		elif self.collisions['right'] and self.direction == 1:
+			self.direction = 0
+		self.move()
