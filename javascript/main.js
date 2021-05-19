adf72feb02ed6d3a0036a14f085c4860965d703d
kotaby <a19carcruval@inspedralbes.cat>
2021-05-19 18:53:40 +0200

    doc pagina web terminada
    aun falta acerla responsive

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//         setTimeout(function(){
+//           $('header').remove();
+//         }, 6100)
+// });
+// $(document).ready(function() {
+//   setTimeout(function(){
+//     var element = document.getElementById("disable");
+//     element.classList.remove("f-scroll")
+//   }, 6000)
+// });
