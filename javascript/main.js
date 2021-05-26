3770f6c334c0746123c58e945cead0a5d2205212
kotaby <a19carcruval@inspedralbes.cat>
2021-05-26 20:41:02 +0200

    integrar javascript a html

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
-        setTimeout(function(){
-          $('header').remove();
-        }, 7100)
-});
-$(document).ready(function() {
-  setTimeout(function(){
-    var element = document.getElementById("disable");
-    element.classList.remove("f-scroll")
-  }, 6000)
-});
-$(document).ready(function() {
-    setTimeout(function(){
-      var element = document.getElementById("loadingwindow");
-      element.classList.add("animate__hinge")
-    }, 6000)
-  });
-$(document).ready(function() {
-    setTimeout(function(){
-      var element = document.getElementById("display-window");
-      element.classList.remove("visivle")
-    }, 1000)
-});
+// $(document).ready(function() {
+//         setTimeout(function(){
+//           $('header').remove();
+//         }, 7100)
+// });
+// $(document).ready(function() {
+//   setTimeout(function(){
+//     var element = document.getElementById("disable");
+//     element.classList.remove("f-scroll")
+//   }, 6000)
+// });
+// $(document).ready(function() {
+//     setTimeout(function(){
+//       var element = document.getElementById("loadingwindow");
+//       element.classList.add("animate__hinge")
+//     }, 6000)
+//   });
+// $(document).ready(function() {
+//     setTimeout(function(){
+//       var element = document.getElementById("display-window");
+//       element.classList.remove("visivle")
+//     }, 1000)
+// });
